2256a12d7656dea724c4209581db3f67881422b5
iyume <iyumelive@gmail.com>
2024-12-20 15:35:42 +0800

    add task async

--- iloveapi/task.py
+++ iloveapi/task.py
@@ -2,13 +2,14 @@ from __future__ import annotations
 
 from pathlib import Path
 from typing import TYPE_CHECKING, Any, TypedDict, cast, overload
-from urllib.parse import urlparse
 
 from typing_extensions import NotRequired, Self
 
-from iloveapi.utils import to_dict
+from iloveapi.utils import get_filename_from_url, to_dict
 
 if TYPE_CHECKING:
+    import httpx
+
     from iloveapi.iloveapi import ILoveApi
     from iloveapi.rest import Rest
     from iloveapi.typing import T_ImageTools, T_PdfTools
@@ -50,22 +51,37 @@ class Task:
             file = Path(file)
         upload_response = self.client.rest.upload(self._server, self._task, file)
         upload_response.raise_for_status()
-        data = upload_response.json()
-        server_filename = data["server_filename"]
-        self._uploaded_files.append(
-            {"server_filename": server_filename, "filename": file.name}
+        return self._add_file(upload_response, file.name)
+
+    async def add_file_async(self, file: str | Path) -> str:
+        if not isinstance(file, Path):
+            file = Path(file)
+        upload_response = await self.client.rest.upload_async(
+            self._server, self._task, file
         )
-        return server_filename
+        upload_response.raise_for_status()
+        return self._add_file(upload_response, file.name)
 
     def add_file_by_url(self, url: str, filename: str | None = None) -> str:
         if filename is None:
             # Use the final path component of the URL as the filename
-            # Not a best practice, or get from Content-Disposition
-            url_parsed = urlparse(url)
-            filename = Path(url_parsed.path).name
+            # Not a best practice, or get from Content-Disposition header
+            filename = get_filename_from_url(url)
         upload_response = self.client.rest.upload_url(self._server, self._task, url)
         upload_response.raise_for_status()
-        data = upload_response.json()
+        return self._add_file(upload_response, filename)
+
+    async def add_file_by_url_async(self, url: str, filename: str | None = None) -> str:
+        if filename is None:
+            filename = get_filename_from_url(url)
+        upload_response = await self.client.rest.upload_url_async(
+            self._server, self._task, url
+        )
+        upload_response.raise_for_status()
+        return self._add_file(upload_response, filename)
+
+    def _add_file(self, response: httpx.Response, filename: str) -> str:
+        data = response.json()
         server_filename = data["server_filename"]
         self._uploaded_files.append(
             {"server_filename": server_filename, "filename": filename}
@@ -79,7 +95,7 @@ class Task:
         rotate: NotRequired[int]
         password: NotRequired[str]
 
-    def process(self, files: list[_File] | None = None, **kwargs: Any) -> None:
+    def _get_process_files(self, files: list[_File] | None) -> list[Rest._File]:
         proc_files: list[Rest._File]
         # Duplicate filename are processed as "{filename}-copy-0"
         if files is None:
@@ -109,7 +125,20 @@ class Task:
                         **file,
                     }
                 )
+        return proc_files
+
+    def process(self, files: list[_File] | None = None, **kwargs: Any) -> None:
+        proc_files = self._get_process_files(files)
         process_response = self.client.rest.process(
+            self._server, self._task, self._tool, proc_files, **kwargs
+        )
+        process_response.raise_for_status()
+
+    async def process_async(
+        self, files: list[_File] | None = None, **kwargs: Any
+    ) -> None:
+        proc_files = self._get_process_files(files)
+        process_response = await self.client.rest.process_async(
             self._server, self._task, self._tool, proc_files, **kwargs
         )
         process_response.raise_for_status()
@@ -123,10 +152,30 @@ class Task:
     def download(self, output_file: str | Path | None = None) -> bytes | None:
         download_response = self.client.rest.download(self._server, self._task)
         download_response.raise_for_status()
+        return self._download(download_response, output_file)
+
+    @overload
+    async def download_async(self, output_file: None = None) -> bytes: ...
+
+    @overload
+    async def download_async(self, output_file: str | Path) -> None: ...
+
+    async def download_async(
+        self, output_file: str | Path | None = None
+    ) -> bytes | None:
+        download_response = await self.client.rest.download_async(
+            self._server, self._task
+        )
+        download_response.raise_for_status()
+        return self._download(download_response, output_file)
+
+    def _download(
+        self, response: httpx.Response, output_file: str | Path | None
+    ) -> bytes | None:
         if output_file is None:
-            return download_response.content
+            return response.content
         if not isinstance(output_file, Path):
             output_file = Path(output_file)
         output_file.parent.mkdir(parents=True, exist_ok=True)
-        output_file.write_bytes(download_response.content)
+        output_file.write_bytes(response.content)
         return None
--- iloveapi/utils.py
+++ iloveapi/utils.py
@@ -1,4 +1,6 @@
+from pathlib import Path
 from typing import Any, Callable, Mapping, Sequence, TypeVar
+from urllib.parse import urlparse
 
 from typing_extensions import Concatenate, ParamSpec
 
@@ -20,3 +22,8 @@ def to_dict(obj: Sequence[TM_co], key: TK) -> dict[TK, TM_co]:
     Considering performance, this function do not use a lambda-based keyselector.
     """
     return {item[key]: item for item in obj}
+
+
+def get_filename_from_url(url: str) -> str:
+    url_parsed = urlparse(url)
+    return Path(url_parsed.path).name
